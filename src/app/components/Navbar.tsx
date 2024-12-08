d02a6ebb707980859f4372030b27a2f59f174de5
dareemrafiq <dareemrafiq100@gmail.com>
2024-12-08 23:18:32 +0500

    first commit

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+      <div className='navbar'>
+          <div className='text'>Dareem Rafiq</div>
+        <a href="#Home-section">Home</a>
+        <a href="#About-section">About</a>
+        <a href="#Contact-section">Contact</a>
+        <section id="Home-section"></section>
+      </div>
+    )
+  }
+  
+  export default Navbar
